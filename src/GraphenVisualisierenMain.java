bf58870890ff6bc589667a55e6dcfb876001b11a
PRitter <PRitter@pats-mbp.fritz.box>
2018-12-20 08:14:27 +0100

    fsd

--- src/GraphenVisualisierenMain.java
+++ src/GraphenVisualisierenMain.java
@@ -13,12 +13,13 @@ public class GraphenVisualisierenMain
 		Adjazenzliste adjazensliste;
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
-		System.out.print("Gib die Graphengroesse ein (muss > 5 sein): ");
+		System.out.print("Gib die Graphengroesse ein (muss > 6 sein): ");
 		int eingabe = Integer.parseInt(br.readLine());
 		adjazensmatrix = new AdjazensmatrixArray(eingabe, false);
 		adjazensliste = new Adjazenzliste(eingabe);
 		adjazensmatrixZufall1 = new AdjazensmatrixArray(eingabe, true);
 		adjazensmatrixZufall2 = new AdjazensmatrixArray(eingabe, true);
+		AdjazensmatrixArray adjazensmatrix1 = new AdjazensmatrixArray(12, false);
 
 		if (eingabe > 5)
 		{
@@ -31,6 +32,7 @@ public class GraphenVisualisierenMain
 			adjazensliste.KanteEinfuegen(2, 5, 8);
 			adjazensliste.KanteEinfuegen(3, 4, 7);
 			adjazensliste.KanteEinfuegen(4, 5, 6);
+			adjazensliste.KanteEinfuegen(5, 6, 3);
 
 			adjazensmatrix.KanteEinfuegen(0, 1, 3);
 			adjazensmatrix.KanteEinfuegen(0, 2, 4);
@@ -41,6 +43,28 @@ public class GraphenVisualisierenMain
 			adjazensmatrix.KanteEinfuegen(2, 5, 8);
 			adjazensmatrix.KanteEinfuegen(3, 4, 7);
 			adjazensmatrix.KanteEinfuegen(4, 5, 6);
+			adjazensmatrix.KanteEinfuegen(5, 6, 3);
+
+			adjazensmatrix1.KanteEinfuegen(0, 1, 3);
+			adjazensmatrix1.KanteEinfuegen(0, 2, 1);
+			adjazensmatrix1.KanteEinfuegen(1, 2, 7);
+			adjazensmatrix1.KanteEinfuegen(1, 3, 3);
+			adjazensmatrix1.KanteEinfuegen(1, 4, 7);
+			adjazensmatrix1.KanteEinfuegen(2, 4, 6);
+			adjazensmatrix1.KanteEinfuegen(2, 5, 2);
+			adjazensmatrix1.KanteEinfuegen(3, 4, 7);
+			adjazensmatrix1.KanteEinfuegen(4, 5, 6);
+
+			adjazensmatrix1.KanteEinfuegen(5, 6, 3);
+			adjazensmatrix1.KanteEinfuegen(5, 11, 5);
+			adjazensmatrix1.KanteEinfuegen(6, 2, 4);
+			adjazensmatrix1.KanteEinfuegen(6, 3, 8);
+			adjazensmatrix1.KanteEinfuegen(7, 10, 3);
+			adjazensmatrix1.KanteEinfuegen(7, 9, 2);
+			adjazensmatrix1.KanteEinfuegen(8, 9, 6);
+			adjazensmatrix1.KanteEinfuegen(10, 9, 8);
+			adjazensmatrix1.KanteEinfuegen(10, 11, 23);
+
 		}
 
 		adjazensmatrix.DebugHilfe();
@@ -51,15 +75,43 @@ public class GraphenVisualisierenMain
 		System.out.println();
 		adjazensmatrixZufall2.DebugHilfe();
 		System.out.println();
+		System.out.println();
 		DijkstraAlgo algoM = new DijkstraAlgo(adjazensmatrix);
+		System.out.println(algoM.ermittleKuerzestenWege(0));
+		System.out.println();
+		DijkstraAlgo algoL = new DijkstraAlgo(adjazensliste);
+		System.out.println(algoL.ermittleKuerzestenWege(0));
+		System.out.println();
+		System.out.println(algoM.ermittleKuerzestenWege(1));
+		System.out.println();
+		System.out.println(algoL.ermittleKuerzestenWege(1));
+		System.out.println();
 		System.out.println(algoM.ermittleKuerzestenWege(2));
+		System.out.println();
+		System.out.println(algoL.ermittleKuerzestenWege(2));
+		System.out.println();
+		System.out.println(algoM.ermittleKuerzestenWege(3));
+		System.out.println();
+		System.out.println(algoL.ermittleKuerzestenWege(3));
+		System.out.println();
+		System.out.println(algoM.ermittleKuerzestenWege(4));
+		System.out.println();
+		System.out.println(algoL.ermittleKuerzestenWege(4));
 		System.out.println();
 		System.out.println(algoM.ermittleKuerzestenWege(5));
 		System.out.println();
-		DijkstraAlgo algoL = new DijkstraAlgo(adjazensliste);
 		System.out.println(algoL.ermittleKuerzestenWege(5));
+		System.out.println();
+		System.out.println(algoM.ermittleKuerzestenWege(6));
+		System.out.println();
+		System.out.println(algoL.ermittleKuerzestenWege(6));
+		System.out.println();
 		
-		
+		DijkstraAlgo algoM1 = new DijkstraAlgo(adjazensmatrix1);
+		System.out.println(algoM1.ermittleKuerzestenWege(0));
+		System.out.println();
+		// DijkstraAlgo algoL1 = new DijkstraAlgo(adjazensliste);
+		// System.out.println(algoL1.ermittleKuerzestenWege(5));
 
 	}
 }
--- src/Test_Algo.java
+++ src/Test_Algo.java
@@ -22,12 +22,12 @@ public class Test_Algo
 
 		DijkstraAlgo algoM = new DijkstraAlgo(adjazensmatrix);
 
-		//assertEquals(algoM.ermittleKuerzestenWege(0).toString(), "{0=0, 1=3, 2=4, 3=6, 4=10, 5=12}");
+		assertEquals(algoM.ermittleKuerzestenWege(0).toString(), "{0=0, 1=3, 2=4, 3=6, 4=10, 5=12}");
 		assertEquals(algoM.ermittleKuerzestenWege(1).toString(), "{0=3, 1=0, 2=7, 3=3, 4=7, 5=13}");
-//		assertEquals(algoM.ermittleKuerzestenWege(2).toString(), "{0=4, 1=7, 2=0, 3=10, 4=6, 5=8}");
-//		assertEquals(algoM.ermittleKuerzestenWege(3).toString(), "{0=6, 1=3, 2=10, 3=0, 4=7, 5=13}");
-//		assertEquals(algoM.ermittleKuerzestenWege(4).toString(), "{0=3, 1=0, 2=7, 3=3, 4=7, 5=13}");
-//		assertEquals(algoM.ermittleKuerzestenWege(5).toString(), "{0=10, 1=7, 2=6, 3=7, 4=0, 5=6}");
+		assertEquals(algoM.ermittleKuerzestenWege(2).toString(), "{0=4, 1=7, 2=0, 3=10, 4=6, 5=8}");
+		assertEquals(algoM.ermittleKuerzestenWege(3).toString(), "{0=6, 1=3, 2=10, 3=0, 4=7, 5=13}");
+		assertEquals(algoM.ermittleKuerzestenWege(4).toString(), "{0=10, 1=7, 2=6, 3=7, 4=0, 5=6}");
+		assertEquals(algoM.ermittleKuerzestenWege(5).toString(), "{0=12, 1=13, 2=8, 3=13, 4=6, 5=0}");
 	}
 
 	@Test
@@ -51,8 +51,8 @@ public class Test_Algo
 		assertEquals(algoL.ermittleKuerzestenWege(1).toString(), "{0=3, 1=0, 2=7, 3=3, 4=7, 5=13}");
 		assertEquals(algoL.ermittleKuerzestenWege(2).toString(), "{0=4, 1=7, 2=0, 3=10, 4=6, 5=8}");
 		assertEquals(algoL.ermittleKuerzestenWege(3).toString(), "{0=6, 1=3, 2=10, 3=0, 4=7, 5=13}");
-		assertEquals(algoL.ermittleKuerzestenWege(4).toString(), "{0=3, 1=0, 2=7, 3=3, 4=7, 5=13}");
-		assertEquals(algoL.ermittleKuerzestenWege(5).toString(), "{0=10, 1=7, 2=6, 3=7, 4=0, 5=6}");
+		assertEquals(algoL.ermittleKuerzestenWege(4).toString(), "{0=10, 1=7, 2=6, 3=7, 4=0, 5=6}");
+		assertEquals(algoL.ermittleKuerzestenWege(5).toString(), "{0=12, 1=13, 2=8, 3=13, 4=6, 5=0}");
 
 	}
 
@@ -62,14 +62,14 @@ public class Test_Algo
 		AdjazensmatrixArray adjazensmatrix = new AdjazensmatrixArray(12, false);
 
 		adjazensmatrix.KanteEinfuegen(0, 1, 3);
-		adjazensmatrix.KanteEinfuegen(0, 2, 4);
+		adjazensmatrix.KanteEinfuegen(0, 2, 1);
 		adjazensmatrix.KanteEinfuegen(1, 2, 7);
 		adjazensmatrix.KanteEinfuegen(1, 3, 3);
 		adjazensmatrix.KanteEinfuegen(1, 4, 7);
 		adjazensmatrix.KanteEinfuegen(2, 4, 6);
-		adjazensmatrix.KanteEinfuegen(2, 5, 8);
+		adjazensmatrix.KanteEinfuegen(2, 5, 2);
 		adjazensmatrix.KanteEinfuegen(3, 4, 7);
-		adjazensmatrix.KanteEinfuegen(4, 5, 6);
+		adjazensmatrix.KanteEinfuegen(4, 5, 6); 
 
 		adjazensmatrix.KanteEinfuegen(5, 6, 3);
 		adjazensmatrix.KanteEinfuegen(5, 11, 5);
@@ -77,14 +77,14 @@ public class Test_Algo
 		adjazensmatrix.KanteEinfuegen(6, 3, 8);
 		adjazensmatrix.KanteEinfuegen(7, 10, 3);
 		adjazensmatrix.KanteEinfuegen(7, 9, 2);
-		adjazensmatrix.KanteEinfuegen(8, 9, 6);
+		adjazensmatrix.KanteEinfuegen(9, 8, 6);
 		adjazensmatrix.KanteEinfuegen(10, 9, 8);
 		adjazensmatrix.KanteEinfuegen(10, 11, 23);
 
 		DijkstraAlgo algoM = new DijkstraAlgo(adjazensmatrix);
 
 		assertEquals(algoM.ermittleKuerzestenWege(0).toString(),
-				"{0=0, 1=3, 2=4, 3=6, 4=10, 5=12, 6=8, 7=43, 8=51, 9=45, 10=40, 11=17}");
+				"{0=0, 1=3, 2=1, 3=6, 4=7, 5=3, 6=6, 7=34, 8=42, 9=36, 10=31, 11=8}");
 		// assertEquals(algoM.ermittleKuerzestenWege(1).toString(), "{0=3, 1=0, 2=7,
 		// 3=3, 4=7, 5=13}");
 		// assertEquals(algoM.ermittleKuerzestenWege(2).toString(), "{0=4, 1=7, 2=0,
